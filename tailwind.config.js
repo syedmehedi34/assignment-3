499888831a73dd976f74d2144e258c48ca5148a8
syedmehedi34 <syedmeehdi34@gmail.com>
2024-08-22 00:59:26 +0600

    hero section done

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     theme: {
+//       extend: {
+//         fontFamily: {
+//           'customs': ['Manrope', 'sans-serif'], // Add your custom font here
+//         },
+//       },
+//     },
+//     plugins: [],
+//   }
